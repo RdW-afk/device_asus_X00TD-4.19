1a64088f2a44a29b14d9b7690de985253c354ece
Mr- 0 <oldcrocs21@gmail.com>
2024-09-12 16:29:01 +0000

    X00TD: Afterlify
    Change-Id: Ie121dd4996d458044a9c14813d5f0adbef7d5e4a
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/afterlife_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-ap2a-userdebug
+    afterlife_X00TD-userdebug
--- afterlife_X00TD.mk
+++ afterlife_X00TD.mk
@@ -7,16 +7,26 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterlifeOS stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+# afterlife official flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+AFTERLIFE_MAINTAINER := Romeo_Delta_Wishkey
+
+# Offline Charging
+USE_PIXEL_CHARGING := true
+
+# disable/enable blur support, default is false
+TARGET_SUPPORTS_BLUR := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := afterlife_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -32,10 +42,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "asus/RU_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+
+# Include our private certificate
+-include vendor/afterlife-priv/keys/keys.mk
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.build.fingerprint=$(BUILD_FINGERPRINT) \
     ro.system.build.fingerprint=$(BUILD_FINGERPRINT) \
     ro.product.build.fingerprint=$(BUILD_FINGERPRINT) \
-    ro.system_ext.build.fingerprint=$(BUILD_FINGERPRINT)
+    ro.system_ext.build.fingerprint=$(BUILD_FINGERPRINT) \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
