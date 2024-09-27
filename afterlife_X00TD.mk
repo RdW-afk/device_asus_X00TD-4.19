9490ef8eea1419ea8591e43c1f01ab6a541cf562
Mr- 0 <oldcrocs21@gmail.com>
2024-09-27 11:52:05 +0000

    X00TD: Afterlify
    Change-Id: I0762cca8ce08b3e2760aae22db3e29ca833ce33f
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- afterlife_X00TD.mk
+++ afterlife_X00TD.mk
@@ -7,16 +7,35 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterlifeOS stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+# Afterlife Maintainer
+AFTERLIFE_MAINTAINER := Romeo
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Face Unlock 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Offline Charging
+USE_PIXEL_CHARGING := true
+
+# disable/enable blur support, default is false
+TARGET_SUPPORTS_BLUR := true
+
+# Gapps
+AFTERLIFE_GAPPS := true
+GAPPS_CORE := true
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := afterlife_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -32,7 +51,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "asus/RU_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_6:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.build.fingerprint=$(BUILD_FINGERPRINT) \
